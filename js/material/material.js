2c0503c5f96252d5305750e10b9a6775902c3072
fernandosg <fsg_tmp@hotmail.com>
2018-02-10 22:52:50 -0600

    Agregando función para manejar el material para PlaneGeometry.

--- js/material/material.js
+++ js/material/material.js
@@ -1,3 +1,31 @@
+function PlaneGeometryMaterial(){
+  var loader= new THREE.TextureLoader();
+
+  function cargar(imagen,resolve){
+    loader.load(imagen,function(textura){
+      var uniforms={
+        "texture":{type:"t",value:textura}
+      };
+      resolve(new THREE.MeshBasicMaterial({map:textura}));
+    },function(xhr){
+
+    });
+  }
+
+  var crear=function(propiedades,resolve){
+    var material;
+    if(propiedades.hasOwnProperty("imagen")){
+      material=cargar(propiedades.imagen,resolve);
+    }else if(propiedades.hasOwnProperty("color")){
+      resolve(new THREE.MeshBasicMaterial(propiedades));
+    }
+  }
+
+  return{
+    crear:crear
+  }
+}
+
 function SphereGeometryMaterial(){
   var loader= new THREE.TextureLoader();
   function crearShaderMaterial(uniforms){
@@ -41,6 +69,10 @@ function GeneradorMaterial(){
         case "SphereGeometry":
           var sphere_geometry_material=new SphereGeometryMaterial();
           sphere_geometry_material.crear(propiedades,resolve);
+          break;
+        case "PlaneGeometry":
+          var plane_geometry_material=new PlaneGeometryMaterial();
+          plane_geometry_material.crear(propiedades,resolve);
           break;
         default:
           resolve(null);
