8e2fbe5ee6edc7d6d836fba084db788f56df5266
fernandosg <fsg_tmp@hotmail.com>
2018-02-10 22:13:01 -0600

    Agregando una nueva funcion, Material, la cual permite generar a partir de un tipo de geometría y un conjunto de propiedades, que tipo de material debe de generarse. Para la carga de imágenes, se implemento async/await y promise en lugar de callbacks.

---
+++ js/material/material.js
@@ -0,0 +1,59 @@
+function SphereGeometryMaterial(){
+  var loader= new THREE.TextureLoader();
+  function crearShaderMaterial(uniforms){
+    return new THREE.ShaderMaterial( {
+      uniforms		: uniforms,
+      vertexShader	: document.getElementById( 'vertex_shader' ).textContent,
+      fragmentShader	: document.getElementById( 'fragment_shader' ).textContent
+    });
+  }
+
+  function crearMeshBasicMaterial(propiedades){
+    return new THREE.MeshBasicMaterial(propiedades);
+  }
+
+  function cargar(imagen,resolve){
+    loader.load(imagen,function(textura){
+      var uniforms={
+        "texture":{type:"t",value:textura}
+      };
+      resolve(crearShaderMaterial(uniforms));
+    },function(xhr){
+
+    });
+  }
+  var crear=function(propiedades,resolve){
+    var material;
+    if(propiedades.hasOwnProperty("imagen")){
+      material=cargar(propiedades.imagen,resolve);
+    }else if(propiedades.hasOwnProperty("color")){
+      resolve(new THREE.MeshBasicMaterial(propiedades));
+    }
+  }
+
+  return{
+    crear:crear
+  }
+}
+
+
+function GeneradorMaterial(){
+  var generar=function(tipo_geometria,propiedades){
+    return new Promise((resolve)=>{
+      var material;
+      switch(tipo_geometria){
+        case "SphereGeometry":
+          var sphere_geometry_material=new SphereGeometryMaterial();
+          sphere_geometry_material.crear(propiedades,resolve);
+          break;
+        default:
+          resolve(null);
+          break;
+      }
+    })
+  }
+
+  return{
+    generar:generar
+  }
+}
